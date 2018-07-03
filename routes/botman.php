4a198c3f5b94f8ac6c687d9c780d1736129938a7
Igor Kabozin <eshaft@gmail.com>
2018-07-03 21:27:13 +0300

    facebook start button

--- routes/botman.php
+++ routes/botman.php
@@ -1,14 +1,19 @@
 <?php
 use App\Http\Controllers\BotManController;
+use BotMan\BotMan\BotMan;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
+$botman->hears('Hi', function (BotMan $bot) {
     $bot->reply('Hello!');
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
-$botman->hears('It just works', function($bot) {
+$botman->hears('It just works', function(BotMan $bot) {
     $bot->reply('Yep 🤘');
+});
+
+$botman->hears('GET_STARTED', function (BotMan $bot) {
+    $bot->reply('Welcome!');
 });
