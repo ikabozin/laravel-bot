3a5a5ca3294cc0978bc04d1eda87670a971bdd90
Igor Kabozin <eshaft@gmail.com>
2018-07-24 11:56:01 +0300

    spatie crawler

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/pdf', function () {
 });
 
 Route::get('/crawler', function () {
-    Crawler::create()
+    return Crawler::create()
         ->setCrawlObserver((new \App\Crawler\SimpleCrawlerObserver))
         ->startCrawling('http://www.github.com');
 });
