115a0cb8d83125fe35da0e7208e441590a2fa772
Igor Kabozin <eshaft@gmail.com>
2018-08-02 19:02:32 +0300

    newsletter

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
@@ -9,5 +10,18 @@ class SubscriberController extends Controller
     public function index()
     {
         return view('subscribe_form');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email'        => 'required',
+        ]);
+
+        Subscriber::create([
+            'email'        => request('email'),
+        ]);
+
+        return response()->json(1, 200);
     }
 }
